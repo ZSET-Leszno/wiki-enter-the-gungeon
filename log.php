01aafdf544a16c6a9018e8938c4d09ccd9e9ea00
Bruczu <brajan.hylla.bh@gmail.com>
2023-02-27 14:57:58 +0100

    .

---
+++ log.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="shortcut icon" href="img/icon.jpg">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="smartphone.css" media="screen and (maxwidth:1024px)">
+    <link rel="stylesheet" href="style.css">
+    <title>Pocket Ammunicon</title>
+</head>
+<body>
+    <header>
+        <div id="logo">
+            <img id="logo_img" src="img/logo.png" alt="logo">
+        </div>
+        <div id="title">
+            <a >Enter the gungeon WIKI "Pocket Ammunicon" by Brajan Hylla & Joanna Muzyka</a>
+        </div>
+        <nav>
+            <a id="link_" href="index.html">START</a>
+            <a id="link_" href="logowanie.php">LOG IN</a>
+        </nav>
+    </header>
+    <main>
+
+    </main>
+    <footer>
+        <a>Strona by Brajan Hylla & Joanna Muzyka</a>
+    </footer>
+</body>
+</html>
